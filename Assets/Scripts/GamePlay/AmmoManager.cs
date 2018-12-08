ae797529a7153c96895cd2e0645acd3bc898df2e
sumankharal <kharalsuman007@gmail.com>
2018-12-08 02:46:44 +0200

    Added quaternions and positions of torpedoLauncher into the list so that the user actions are executed after cooldown is completed if user fires during the cooldown.

--- Assets/Scripts/GamePlay/AmmoManager.cs
+++ Assets/Scripts/GamePlay/AmmoManager.cs
@@ -13,14 +13,15 @@ public class AmmoManager : MonoBehaviour
     public float torpedoCoolTime;                // Total amount of time that takes for weapon to cool down.
     public float torpedoCoolRate;                // Rate of time in which the weapon cools down. 
     public Transform torpedoEndTranform;        // Transfrom form where the ammo is lunched.
+    public Transform torpedoVisualLuancher;     // Visual display of torpedoLuncher to face the direction where mouse was previously clicked
     public ObjectPooler ammoPool;               // Generic class to instantiate array of gameobject in a pool
     public GameObject[] ammoPrefabs;            //Variations of ammos that can be created from the ObjectPooler class.
     #endregion
 
     #region Private Fields	
-
+    private List<Quaternion> torpedoQuaternions = new List<Quaternion>();   // List of quaternions to save the rotation of invisible torpedo towards mouse direction
+    private List<Vector3> torpedoTipPositions = new List<Vector3>();        // List of positions of the tip of invisible torpedo.
     bool isCooled = true;
-
     #endregion
 
     private void Awake()
@@ -34,11 +35,35 @@ public class AmmoManager : MonoBehaviour
     }
     private void Update()
     {
-        // User can only fire when left mouse button is clicked and when luncher is cooled.
-        if (Input.GetMouseButtonDown(0) && isCooled == true)
+
+        if (Input.GetMouseButtonDown(0))
         {
-            ammoPool.CreateObject(torpedoEndTranform.position, torpedoEndTranform.rotation);
-            isCooled = false;
+            // Adding the last rotation of invisible torpedo and position of invisible torpedo end towards the mouse position when clicked
+            torpedoQuaternions.Add(torpedoEndTranform.rotation);
+            torpedoTipPositions.Add(torpedoEndTranform.position);
+        }
+
+        // Firing the ammo only when the torpedo has cooled down
+        if (isCooled)
+        {
+            // Checking if the user has previouse clicked the left mouse button.
+            if (!(torpedoQuaternions.Count <= 0))
+            {
+                // Getting the rotation and position of torpedo from the first elements of lists of quaternion and position
+                Quaternion torpedoQuaternion = torpedoQuaternions[0];
+                Vector3 torpedoTipPosition = torpedoTipPositions[0];
+
+                // Removing the rotation and position of torpedo from the first elements of lists as they are already stored.
+                torpedoQuaternions.RemoveAt(0);
+                torpedoTipPositions.RemoveAt(0);
+
+                // Creating object pool from the temporarily saved quaternion and position.
+                ammoPool.CreateObject(torpedoTipPosition, torpedoQuaternion);
+
+                // Setting the visual torpedo luncher direction and position to mimic facing the last clicked direction of mouse.
+                torpedoVisualLuancher.rotation = torpedoQuaternion;
+                isCooled = false;
+            }
         }
 
         if (!isCooled)
